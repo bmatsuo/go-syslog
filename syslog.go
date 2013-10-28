e7c0cf9a4561e3331bbdcc2dbfa1c8c1ab0bc618
Bryan Matsuo <bryan.matsuo@gmail.com>
2013-10-28 01:00:20 -0700

    split out message queue stuff

--- syslog.go
+++ syslog.go
@@ -261,54 +261,6 @@ func NewSyslog(conn net.Conn, appender Appender, facility Priority, level Priori
 	return syslog, nil
 }
 
-// not threadsafe. the caller must be theradsafe
-type messageQueue struct {
-	length int
-	head   *message
-	tail   *message
-}
-
-var errEmpty = fmt.Errorf("empty")
-var errHighwater = fmt.Errorf("highwater")
-
-func (q *messageQueue) Dequeue() (*message, error) {
-	if q.length == 0 {
-		return nil, errEmpty
-	}
-	msg := q.head
-	q.head = q.head.next
-	q.length--
-	if q.head == nil {
-		q.tail = nil
-	}
-	return msg, nil
-}
-
-func (q *messageQueue) Enqueue(highwater int, msg *message) error {
-	// check msg first
-	if msg == nil {
-		return fmt.Errorf("nil msg")
-	}
-	if msg.next != nil {
-		return fmt.Errorf("msg already enqueued")
-	}
-
-	if highwater <= q.length {
-		return errHighwater
-	}
-
-	if q.head == nil {
-		q.head = msg
-	}
-	if q.tail != nil {
-		q.tail.next = msg
-	}
-	q.tail = msg
-	q.length++
-
-	return nil
-}
-
 func (syslog *Syslog) start() error {
 	term := <-syslog.termlock
 	if term != nil {
